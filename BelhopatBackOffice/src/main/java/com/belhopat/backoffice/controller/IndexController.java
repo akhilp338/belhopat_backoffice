3cdf545c36d12a72a30fd636354038a1c18c7a53
sujith <sujithkvclt@gmail.com>
2016-04-12 20:27:56 +0530

    code cleaned

--- BelhopatBackOffice/src/main/java/com/belhopat/backoffice/controller/IndexController.java
+++ BelhopatBackOffice/src/main/java/com/belhopat/backoffice/controller/IndexController.java
@@ -1,11 +1,8 @@
 package com.belhopat.backoffice.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.belhopat.backoffice.repository.UserRepository;
 
 @Controller
 @RequestMapping("/*")
