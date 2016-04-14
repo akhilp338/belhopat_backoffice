b181744984fc7a0c9c46651bd38d891697b5abc0
Prince <princeg2qburst.com>
2016-04-14 11:56:08 -0700

    commit for frontend major changes and inner pages

--- BelhopatBackOffice/src/main/webapp/app/shared/CoreService.js
+++ BelhopatBackOffice/src/main/webapp/app/shared/CoreService.js
@@ -1,17 +1,17 @@
 (function (angular) {
-    var Core_Service = function ($rootScope,Core_HttpRequest,Base64, $state,$cookieStore,$sessionStorage, $http, $q, $timeout) {
+    var Core_Service = function ($rootScope, Core_HttpRequest, Base64, $state, $cookieStore, $sessionStorage, $http, $q, $timeout) {
         var service = this;
 
         service.login = function (data) {
             var deferred = $q.defer();
-            var user={};
-            user.username=data.username;
-            user.password=data.password;
-            Core_HttpRequest.post("api/login",user)
+            var user = {};
+            user.username = data.username;
+            user.password = data.password;
+            Core_HttpRequest.post("api/login", user)
                     .then(function (response) {
                         if (response.status == 200) {
-                               deferred.resolve(response.data);
- 
+                            deferred.resolve(response.data);
+
                         }
                     }, function (response) {
                         response.data = false;
@@ -52,63 +52,77 @@
         };
 
         service.sendPassword = function (data) {
-           var url = "api/forgotPassword"
-           Core_HttpRequest.post(url,data);
+            var deferred = $q.defer();
+            var url = "api/forgotPassword"
+            Core_HttpRequest.post(url, data).then(function (res) {
+                if (res.success) {
+                    deferred.resolve(res);
+                } else {
+                    deferred.reject(res);
+                }
+            }, function (error) {
+                deferred.reject(error);
+            });
+            return deferred.promise;
         };
         service.SetCredentials = function (username, password) {
             var authdata = Base64.encode(username + ':' + password);
-  
+
             $rootScope.globals = {
                 currentUser: {
                     username: username,
                     authdata: authdata
                 }
             };
-            $sessionStorage.auth =username;
+            $sessionStorage.auth = username;
             $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata; // jshint ignore:line
             $cookieStore.put('globals', $rootScope.globals);
         };
-  
+
         service.ClearCredentials = function () {
-        	var deferred = $q.defer();
+            var deferred = $q.defer();
             $rootScope.globals = {};
             Core_HttpRequest.post("api/logout")
-            .then(function (response) {
-                if (response.status == 200) {
-                       deferred.resolve(response.data);
-                }
-            }, function (response) {
-                response.data = false;
-                deferred.reject(response.data);
-            });
+                    .then(function (response) {
+                        if (response.status == 200) {
+                            deferred.resolve(response.data);
+                        }
+                    }, function (response) {
+                        response.data = false;
+                        deferred.reject(response.data);
+                    });
             $cookieStore.remove('globals');
             $http.defaults.headers.common.Authorization = 'Basic ';
         };
-        
-        service.getAllLookupValues = function(url){
-        	var deferred = $q.defer();
+
+        service.getAllLookupValues = function (url) {
+            var deferred = $q.defer();
             Core_HttpRequest.post(url)
                     .then(function (response) {
                         deferred.resolve(response)
                     }, function (error) {
-                    	deferred.reject(error)
+                        deferred.reject(error)
                     });
             return deferred.promise;
-        }
-        
-        service.candidateRegister = function(url,postData){
-        	var deferred = $q.defer();
-        	console.log(postData);
-            Core_HttpRequest.postHttp(url,postData)
+        };
+
+        service.candidateRegister = function (url, postData) {
+            var deferred = $q.defer();
+            console.log(postData);
+            Core_HttpRequest.postHttp(url, postData)
                     .then(function (response) {
                         deferred.resolve(response)
                     }, function (error) {
-                    	deferred.reject(error)
+                        deferred.reject(error)
                     });
             return deferred.promise;
-        }
+        };
+        
+        service.sweetAlert = function(congrats,message,type){
+            swal(congrats, message, type)
+        };
     };
-    Core_Service.$inject = ['$rootScope','Core_HttpRequest','Base64', '$state', '$cookieStore','$sessionStorage','$http', '$q', '$timeout'];
+    Core_Service.$inject = ['$rootScope', 'Core_HttpRequest', 'Base64', '$state', '$cookieStore', '$sessionStorage', '$http', '$q', '$timeout'];
     angular.module('app.common')
             .service('Core_Service', Core_Service);
 })(angular);
