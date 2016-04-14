619e9152cf286c09a77082c287b8bd95e05b7693
Sreekesh <sreekesh.d@gmail.com>
2016-04-14 16:18:39 +0530

    Updating Constants.java to include updated key values of lookup

--- BelhopatBackOffice/src/main/java/com/belhopat/backoffice/util/Constants.java
+++ BelhopatBackOffice/src/main/java/com/belhopat/backoffice/util/Constants.java
@@ -3,9 +3,9 @@ package com.belhopat.backoffice.util;
 public class Constants {
 
 	/* lookup key */
-	public static final String DIVISION = "division";
-	public static final String DESIGNATION = "designation";
-	public static final String PURPOSE = "purpose";
-	public static final String BLOOD_GROUP = "bloodGroup";
-	public static final String EMPLOYMENT_STATUS = "employmentStatus";
+	public static final String DIVISION = "DIV";
+	public static final String DESIGNATION = "DESIG";
+	public static final String PURPOSE = "PURPO";
+	public static final String BLOOD_GROUP = "BG";
+	public static final String EMPLOYMENT_STATUS = "ESTATS";
 }
