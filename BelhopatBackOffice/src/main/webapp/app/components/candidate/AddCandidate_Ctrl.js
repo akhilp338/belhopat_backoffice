8dbcb4e69aeb9a5dd5ce79aff727e83b0251d02a
Prince <princeg2qburst.com>
2016-04-15 14:20:11 -0700

    commit for candidate view

--- BelhopatBackOffice/src/main/webapp/app/components/candidate/AddCandidate_Ctrl.js
+++ BelhopatBackOffice/src/main/webapp/app/components/candidate/AddCandidate_Ctrl.js
@@ -1,10 +1,14 @@
 (function () {
     var AddCandidate_Ctrl = function ($scope, $state, $rootScope, Core_Service, $timeout, Core_HttpRequest, validationService) {
         var vm = this;
-                vs = new validationService({
-                    controllerAs: vm
-                });
-
+        vs = new validationService({
+            controllerAs: vm
+        });
+        vm.mainSkillList = [{id: 1, skill: 'javascript'}, {id: 2, skill: 'java'}, {id: 3, skill: 'css'}, {id: 4, skill: 'c++'}, {id: 5, skill: 'c'}, {id: 6, skill: 'html'}, {id: 7, skill: 'cobol'}]
+        vm.mainSelectedSkillList = [];
+        vm.subSelectedSkillList = [];
+        vm.deSelectedSkills = [];
+        vm.confirmedSelectionItems = [];
         vm.registration = {};
         vs.setGlobalOptions({
             debounce: 1500,
@@ -13,17 +17,27 @@
             preValidateFormElements: false,
             displayOnlyLastErrorMsg: true
         });
+        vm.addSkills = function () {
+            vm.mainSelectedSkillList = vm.mainSelectedSkillList.concat(vm.subSelectedSkillList);
+            vm.removeFromMainListArray(vm.getIndexesToRemove(vm.mainSkillList, vm.subSelectedSkillList));
+            console.log(vm.confirmedSelectionItems);
+        };
+        vm.removeSkills = function () {
+            vm.mainSkillList = vm.mainSkillList.concat(vm.deSelectedSkills);
+            vm.removeFromSelectedListArray(vm.getIndexesToRemove(vm.mainSelectedSkillList, vm.deSelectedSkills));
+            console.log(vm.confirmedSelectionItems);
+        };
 
-          vm.isCheckboxEnable = false;
-          vm.urlForLookups = "api/candidate/getDropDownData";
-       Core_Service.getAllLookupValues(vm.urlForLookups)
-        .then( function(response) {
-           console.log(response)
-           vm.lookups = response.data;
-        },function(error){
-        	
-        });
-        
+        vm.isCheckboxEnable = false;
+        vm.urlForLookups = "api/candidate/getDropDownData";
+        Core_Service.getAllLookupValues(vm.urlForLookups)
+                .then(function (response) {
+                    console.log(response)
+                    vm.lookups = response.data;
+                }, function (error) {
+
+                });
+
         $scope.steps = [
             'Step 1: Personal Information',
             'Step 2: Employment Details',
@@ -78,7 +92,7 @@
             }
             Core_Service.calculateSidebarHeight();
         };
-       
+
         $rootScope.active = 'candidate';
         vm.copyAddress = function () {
             console.log(vm.registration.permenant)
@@ -104,24 +118,55 @@
                 vm.isCheckboxEnable = false;
             }
         };
-        vm.addCandidate = function(){
+        vm.addCandidate = function () {
             $state.go("coreuser.candidate.add");
         };
-        
-        vm.candidateRegister = function(){
-        	console.log(vm.registration);
-        	vm.registerUrl = "api/candidate/saveOrUpdateCandidate";
-            Core_Service.candidateRegisterImpl(vm.urlForRegister,vm.registration)
-            .then( function(response) {
-               console.log(response)
-            },function(error){
-            	
-            });
+
+        vm.candidateRegister = function () {            
+            vm.registerUrl = "api/candidate/saveOrUpdateCandidate";
+            Core_Service.candidateRegisterImpl(vm.urlForRegister, vm.registration)
+                    .then(function (response) {
+                        console.log(response)
+                    }, function (error) {
+
+                    });
         };
-        
+
+        vm.getIndexesToRemove = function (array, data) {
+            var indexes = [];
+            for (var i = 0; i < data.length; i++) {
+                for (var j = 0; j < array.length; j++) {
+                    if (data[i].id == array[j].id) {
+                        indexes.push(j);
+                    }
+                }
+            }
+            return  indexes;
+        };
+
+        vm.removeFromSelectedListArray = function (indexes) {
+            var selected = [];
+            for (var i = indexes.length - 1; i >= 0; i--)
+                vm.mainSelectedSkillList.splice(indexes[i], 1);            
+            vm.deSelectedSkills = [];
+            for(var j = 0; j<vm.mainSelectedSkillList.length; j++){
+                selected.push(vm.mainSelectedSkillList[j].id)
+            }
+            vm.confirmedSelectionItems = selected;
+        };
+        vm.removeFromMainListArray = function (indexes) {
+            var selected = [];
+            for (var i = indexes.length - 1; i >= 0; i--)
+                vm.mainSkillList.splice(indexes[i], 1);            
+            vm.subSelectedSkillList = [];
+            for(var j = 0; j<vm.mainSelectedSkillList.length; j++){
+                selected.push(vm.mainSelectedSkillList[j].id)
+            }
+            vm.confirmedSelectionItems = selected;
+        };
         Core_Service.calculateSidebarHeight();
     };
-    
+
     AddCandidate_Ctrl.$inject = ["$scope", '$state', '$rootScope', 'Core_Service', '$timeout', 'Core_HttpRequest', 'validationService'];
     angular.module('coreModule')
             .controller('AddCandidate_Ctrl', AddCandidate_Ctrl);
