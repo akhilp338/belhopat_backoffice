29222165b09c4464e66b1a179b3b68860343f3c0
Sreekesh D <sreekeshd@qburst.com>
2016-05-01 23:37:05 +0530

    Minor

--- BelhopatBackOffice/DB-Scripts/Iteration_2/Lookup_Detail_updates_Client.sql
+++ BelhopatBackOffice/DB-Scripts/Iteration_2/Lookup_Detail_updates_Client.sql
@@ -2,7 +2,7 @@ INSERT INTO `belhopat_backoffice`.`LookupDetail`
 (`code`,`description`,`lookup_id`)
 VALUES
 ('ACTIVE','Active',(SELECT id FROM `belhopat_backoffice`.`Lookup` WHERE lookupKey ='CLSTATS')),
-('INACTIVE','INACTIVE',(SELECT id FROM `belhopat_backoffice`.`Lookup` WHERE lookupKey ='CLSTATS'));
+('INACTIVE','Inactive',(SELECT id FROM `belhopat_backoffice`.`Lookup` WHERE lookupKey ='CLSTATS'));
 
 INSERT INTO `belhopat_backoffice`.`LookupDetail`
 (`code`,`description`,`lookup_id`)
