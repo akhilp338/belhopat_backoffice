ce687d9d77dd49123f87d0b8ceffbb274624fb35
Shinto <iamshintomjose@gmail.com>
2016-05-22 14:56:07 +0530

    user and roles DB changes

---
+++ BelhopatBackOffice/src/main/java/com/belhopat/backoffice/model/MasterRoles.java
@@ -0,0 +1,41 @@
+package com.belhopat.backoffice.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Mst_Roles")
+public class MasterRoles {
+
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String roleName;
+	
+	@ManyToMany(mappedBy="roles")
+    private Set<User> users = new HashSet<User>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+}
